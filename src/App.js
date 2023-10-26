b0fce5b922ddf85a95ac6fb9762e92e8c1c6d72b
Santhakumar99 <santhakumar9399@gmail.com>
2023-10-26 23:51:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Menubar from './components/Menubar'
+function App() {
+  return (
+    <div className="App">
+<Menubar/>
+    </div>
+  );
+}
+
+export default App;
